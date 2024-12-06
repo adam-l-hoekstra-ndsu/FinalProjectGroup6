3266d47a046607ffb439429be5fdded6f101f753
Johnnie Zaharia <johnnie.zaharia@ndus.edu>
2024-12-05 21:51:13 -0600

    Application DB and model

---
+++ Data/FinalProjectGroup6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectGroup6.Models;
+
+namespace FinalProjectGroup6.Data
+{
+    public class FinalProjectGroup6Context : DbContext
+    {
+        public FinalProjectGroup6Context (DbContextOptions<FinalProjectGroup6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FinalProjectGroup6.Models.Application> Application { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using FinalProjectGroup6.Components;
+﻿using FinalProjectGroup6.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using FinalProjectGroup6.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<FinalProjectGroup6Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FinalProjectGroup6Context") ?? throw new InvalidOperationException("Connection string 'FinalProjectGroup6Context' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
