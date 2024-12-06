8f146adb91495efb96b27bc28c2534bf9efcc7b8
adam-l-hoekstra-ndsu <adam.l.hoekstra@ndsu.edu>
2024-12-06 14:59:09 -0600

    Updated animal shelter class, added message class. Other misc. changes.

--- Migrations/20241206204217_mssql.local_migration_717.Designer.cs
+++ Migrations/20241206204217_mssql.local_migration_717.Designer.cs
@@ -4,6 +4,7 @@ using FinalProjectGroup6.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinalProjectGroup6.Migrations
 {
     [DbContext(typeof(FinalProjectGroup6Context))]
-    partial class FinalProjectGroup6ContextModelSnapshot : ModelSnapshot
+    [Migration("20241206204217_mssql.local_migration_717")]
+    partial class mssqllocal_migration_717
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241206204217_mssql.local_migration_717.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProjectGroup6.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_717 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "AnimalShelter",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "AnimalShelter",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "AnimalShelter");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "AnimalShelter");
+        }
+    }
+}
