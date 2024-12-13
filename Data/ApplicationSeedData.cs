c9179077872080ec2061ca52665d1a8ac957db8f
Johnnie Zaharia <johnnie.zaharia@ndus.edu>
2024-12-13 01:25:27 -0600

    Image, formatting changes, empty seed data class for applications

---
+++ Data/ApplicationSeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProjectGroup6.Models;
+namespace FinalProjectGroup6.Data;
+
+public class ApplicationSeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using var context = new FinalProjectGroup6Context(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<FinalProjectGroup6Context>>());
+
+        if (context == null || context.Application == null)
+        {
+            throw new NullReferenceException(
+                "Null FinalProjectGroup6Context or Student DbSet");
+        }
+
+        if (context.Application.Any())
+        {
+            return;
+        }
+
+        context.Application.AddRange(
+            new Application
+            {
+                
+
+
+            },
+            new Application
+            {
+
+            },
+            new Application
+            {
+
+            },
+            new Application
+            {
+
+            });
+
+        context.SaveChanges();
+    }
+}
+
